69fc8624b5a11956ec6b96dfeab756ef48f9648c
Shima Al Tahr <abdullahsheama@gmail.com>
2026-06-12 19:35:26 +0300

    add secret info to env

--- hpap/settings.py
+++ hpap/settings.py
@@ -148,9 +148,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "elahosten@gmail.com"
-EMAIL_HOST_PASSWORD = "xyuv zcrz szcf oras"
-DEFAULT_FROM_EMAIL = "elahosten@gmail.com"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
